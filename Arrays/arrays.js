1e8688954b267822ac9cad4ba2f01990867a4ce1
Ognjen Gavric <ognjengavric2@gmail.com>
2022-10-24 17:20:06 +0200

    Finish last exercises

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -21,9 +21,12 @@ console.log(printNumDivisibileBy3);
 
 //What is the index of number 24 in the following array? 
 // [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
-var arr = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
-console.log("Index of number " + arr[0][3] + " is arr[0][3]");
-
 // Using console.log:
 // ● Display the 3rd element of the array,
 // ● Display the 2nd element of the 3rd element.
+var arr = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+console.log("Index of number " + arr[0][3] + " is arr[0][3]");
+console.log("3rd element of the array is [" + arr[2] + "]");
+console.log("2nd element of the 3rd element [" + arr[2][1] + "]");
+
+
